3f9ccd54b92e92988fd3fcfa9b4a70740bf3a65a
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-18 18:48:51 +0300

    Identify button resources in QuizActivity

--- android/src/main/java/com/github/ematiyuk/catsquiz/QuizActivity.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/QuizActivity.java
@@ -1,13 +1,20 @@
 package com.github.ematiyuk.catsquiz;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class QuizActivity extends AppCompatActivity {
+public class QuizActivity extends Activity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
     }
 }
