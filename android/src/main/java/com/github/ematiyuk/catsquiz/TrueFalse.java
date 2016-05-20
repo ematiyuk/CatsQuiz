a746c0c51cd169bb31ed4a184f3425b3ad7fc2e3
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-20 12:42:00 +0300

    Update(TrueFalse): add mAnswer field

--- android/src/main/java/com/github/ematiyuk/catsquiz/TrueFalse.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/TrueFalse.java
@@ -2,10 +2,12 @@ package com.github.ematiyuk.catsquiz;
 
 public class TrueFalse {
     private int mQuestion;
+    private int mAnswer;
     private boolean mTrueQuestion;
 
-    public TrueFalse(int question, boolean trueQuestion) {
+    public TrueFalse(int question, int answer, boolean trueQuestion) {
         this.mQuestion = question;
+        this.mAnswer = answer;
         this.mTrueQuestion = trueQuestion;
     }
 
@@ -23,5 +25,13 @@ public class TrueFalse {
 
     public boolean isTrueQuestion() {
         return mTrueQuestion;
+    }
+
+    public int getAnswer() {
+        return mAnswer;
+    }
+
+    public void setAnswer(int answer) {
+        this.mAnswer = answer;
     }
 }
