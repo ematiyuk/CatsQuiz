67751c2e55bb3cfc1c0ff42227f94d98572fa2a6
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-23 17:02:13 +0300

    Enable cheating in CheatActivity

--- android/src/main/java/com/github/ematiyuk/catsquiz/CheatActivity.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/CheatActivity.java
@@ -2,10 +2,17 @@ package com.github.ematiyuk.catsquiz;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CheatActivity extends Activity {
 
     public static final String EXTRA_ANSWER_IS_TRUE = "com.github.ematiyuk.catsquiz.answer_is_true";
+
+    private TextView mAnswerTextView;
+    private Button mShowAnswer;
+
     private boolean mAnswerIsTrue;
 
     @Override
@@ -14,5 +21,18 @@ public class CheatActivity extends Activity {
         setContentView(R.layout.activity_cheat);
 
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
+        mAnswerTextView = (TextView) findViewById(R.id.answerTextView);
+        mShowAnswer = (Button) findViewById(R.id.showAnswerButton);
+
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mAnswerIsTrue)
+                    mAnswerTextView.setText(R.string.true_button);
+                else
+                    mAnswerTextView.setText(R.string.false_button);
+            }
+        });
     }
 }
