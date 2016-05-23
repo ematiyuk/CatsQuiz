bda0f6a81e6e0be5b186c4257e18ca91aef7d25f
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-23 16:33:56 +0300

    Add a constant

--- android/src/main/java/com/github/ematiyuk/catsquiz/CheatActivity.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/CheatActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class CheatActivity extends Activity {
 
+    public static final String EXTRA_ANSWER_IS_TRUE = "com.github.ematiyuk.catsquiz.answer_is_true";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
