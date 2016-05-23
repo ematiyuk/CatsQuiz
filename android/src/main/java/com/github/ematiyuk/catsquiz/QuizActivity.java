eac646a2436040f7d53b643f9b17fdec79810ad1
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-23 16:38:10 +0300

    Put an extra on the intent

--- android/src/main/java/com/github/ematiyuk/catsquiz/QuizActivity.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/QuizActivity.java
@@ -162,6 +162,8 @@ public class QuizActivity extends Activity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(QuizActivity.this, CheatActivity.class);
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+                intent.putExtra(CheatActivity.EXTRA_ANSWER_IS_TRUE, answerIsTrue);
                 startActivity(intent);
             }
         });
