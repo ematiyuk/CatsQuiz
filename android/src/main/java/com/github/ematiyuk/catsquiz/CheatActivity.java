4738a2031d4888aca643607009531e035b74b6b1
Eugene Matiyuk <ematiyuk@gmail.com>
2016-05-23 17:50:40 +0300

    Set a result in CheatActivity

--- android/src/main/java/com/github/ematiyuk/catsquiz/CheatActivity.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/CheatActivity.java
@@ -1,6 +1,7 @@
 package com.github.ematiyuk.catsquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,11 +10,18 @@ import android.widget.TextView;
 public class CheatActivity extends Activity {
 
     public static final String EXTRA_ANSWER_IS_TRUE = "com.github.ematiyuk.catsquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOWN = "com.github.ematiyuk.catsquiz.answer_shown";
 
     private TextView mAnswerTextView;
     private Button mShowAnswer;
 
     private boolean mAnswerIsTrue;
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +40,11 @@ public class CheatActivity extends Activity {
                     mAnswerTextView.setText(R.string.true_button);
                 else
                     mAnswerTextView.setText(R.string.false_button);
+                setAnswerShownResult(true); 
             }
         });
+
+        // answer will not be shown until the user presses the button
+        setAnswerShownResult(false);
     }
 }
