485f9ff56533138ec62c84c3f6ea9dae4a2259fe
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-02 22:51:57 +0300

    Add EXTRA keys to PieChartActivity

--- android/src/main/java/com/github/ematiyuk/catsquiz/PieChartActivity.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/PieChartActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class PieChartActivity extends Activity {
+    public static final String EXTRA_CORRECT_ANSWERS_NUMBER = "com.github.ematiyuk.catsquiz.correct_answers_number";
+    public static final String EXTRA_INCORRECT_ANSWERS_NUMBER = "com.github.ematiyuk.catsquiz.incorrect_answers_number";
+    public static final String EXTRA_CHEATED_ANSWERS_BANK = "com.github.ematiyuk.catsquiz.cheated_answers_bank";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
