f01b34c5618a1216bcce79d44000d33dab040fda
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-02 22:47:59 +0300

    Add PieChartActivity class

---
+++ android/src/main/java/com/github/ematiyuk/catsquiz/PieChartActivity.java
@@ -0,0 +1,12 @@
+package com.github.ematiyuk.catsquiz;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PieChartActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piechart);
+    }
+}
