b36cf910c8c3746e8b2215bf971318c295186f02
Eugene Matiyuk <ematiyuk@gmail.com>
2016-06-02 23:02:44 +0300

    Get extra from Intent in PieChartActivity

--- android/src/main/java/com/github/ematiyuk/catsquiz/PieChartActivity.java
+++ android/src/main/java/com/github/ematiyuk/catsquiz/PieChartActivity.java
@@ -8,9 +8,15 @@ public class PieChartActivity extends Activity {
     public static final String EXTRA_INCORRECT_ANSWERS_NUMBER = "com.github.ematiyuk.catsquiz.incorrect_answers_number";
     public static final String EXTRA_CHEATED_ANSWERS_BANK = "com.github.ematiyuk.catsquiz.cheated_answers_bank";
 
+    private int mCorrectAnswersNumber = 0;
+    private int mIncorrectAnswersNumber = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piechart);
+
+        mCorrectAnswersNumber = getIntent().getIntExtra(EXTRA_CORRECT_ANSWERS_NUMBER, 0);
+        mIncorrectAnswersNumber = getIntent().getIntExtra(EXTRA_INCORRECT_ANSWERS_NUMBER, 0);
     }
 }
